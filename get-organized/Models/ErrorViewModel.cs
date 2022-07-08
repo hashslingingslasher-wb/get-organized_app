d1ac37028a98585585e08d04684ae410190f90d6
hashslingingslasher-wb <wjamesbarlow@gmail.com>
2022-07-07 18:47:07 -0700

    initial setup

---
+++ get-organized/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace get_organized.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
